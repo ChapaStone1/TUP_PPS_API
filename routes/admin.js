35733803518ea072b06e43a03d428b59a154e8eb
Juan Chaparro <chapaPR@gmail.com>
2025-06-28 00:39:31 -0300

    patch

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,7 @@ router.use(verificarToken)
 
 router.delete('/eliminar-paciente/:id', controller.eliminarPaciente)
 router.post('/register-medico', controller.cargarMedico)
-router.put('/habilitacion-medico/:id', controller.cambiarHabilitacionMedico)
+router.patch('/habilitacion-medico/:id', controller.cambiarHabilitacionMedico)
 router.get('/all-medicos', controller.listarMedicos)
 
 module.exports = router
