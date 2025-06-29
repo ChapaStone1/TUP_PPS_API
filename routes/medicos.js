5c4d001f6115ef81bfa4d1a5951ed1d30a447a9b
Juan Chaparro <chapaPR@gmail.com>
2025-06-29 12:21:23 -0300

    readme

--- routes/medicos.js
+++ routes/medicos.js
@@ -7,8 +7,8 @@ router.use(verificarToken)
 
 router.get('/mi-perfil', controller.obtenerPerfil)
 router.put('/mi-perfil', controller.actualizarPerfil)
-router.get('/buscar-paciente/:dni', controller.buscarPacientePorDNI) // query dni
-router.get('/all-pacientes', controller.allPacientes) // query params, dni, limit, offset.
+router.get('/buscar-paciente/:dni', controller.buscarPacientePorDNI) // query dni, arme un endpoint distinto porque este me trae la historia clinica
+router.get('/all-pacientes', controller.allPacientes) // query params, dni, limit, offset
 router.post('/cargar-consulta/:id', controller.cargarConsulta) // query id
 router.get('/historia-clinica/:id', controller.verHistoriaClinica) // query id
 router.get('/especialidades', controller.obtenerEspecialidades) 
