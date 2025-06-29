d5ff82260f5e7eb4308e1861c104941f42abf64d
Juan Chaparro <chapaPR@gmail.com>
2025-06-28 21:56:08 -0300

    ultimos cambios

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,7 @@ router.post('/register-medico', controller.cargarMedico)
 router.patch('/habilitacion-medico/:id', controller.cambiarHabilitacionMedico) //query id
 router.get('/all-medicos', controller.listarMedicos)
 router.get('/all-users', controller.allUsers) //query params, dni, limit, offset.
-router.get('/reset-password/:id', controller.resetearPassword) //query id
+router.patch('/reset-password/:id', controller.resetearPassword) //query id
 
 
 module.exports = router
